f270e5f75e1ee3efda1926239db604d24cf9c318
Jem Mackay <jem@swarmtv.org>
2014-02-04 05:36:46 +0000

    places email verification log in on the "public : home" page

--- private/codeigniter/application/views/verification_success.php
+++ private/codeigniter/application/views/verification_success.php
@@ -29,6 +29,6 @@
 
 	<h1>Your email address was successfully verified.</h1>
 	
-	<p>Thank you for joining the Digital Dialogues community.</p><p><a href="<?php echo base_url(); ?>index.php/pages/view/public/home">public : Home</a>&nbsp;|&nbsp;<a href="<?php echo base_url(); ?>index.php/pages/view/sandpit/home">Sandpit</a>&nbsp;|&nbsp;<a href="<?php echo base_url(); ?>index.php/pages/view/help/home">Help</a>&nbsp;|&nbsp;<a href="<?php echo base_url() . 'index.php/verifylogin/index/pages/' . $group . '/' . $title; ?>">Log In</a></p>
+	<p>Thank you for joining the Digital Dialogues community.</p><p><a href="<?php echo base_url(); ?>index.php/pages/view/public/home">public : Home</a>&nbsp;|&nbsp;<a href="<?php echo base_url(); ?>index.php/pages/view/sandpit/home">Sandpit</a>&nbsp;|&nbsp;<a href="<?php echo base_url(); ?>index.php/pages/view/help/home">Help</a>&nbsp;|&nbsp;<a href="<?php echo base_url() . 'index.php/verifylogin/index/pages/public/home">Log In</a></p>
   </body>
 </html>
