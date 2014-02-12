4a252baeddc068eb69be7f94136fef8b4f8db6c2
Jem Mackay <jem@swarmtv.org>
2014-02-12 09:40:32 +0000

    corrects the rss feed

--- private/codeigniter/application/views/messages_view.php
+++ private/codeigniter/application/views/messages_view.php
@@ -1,6 +1,6 @@
 <div id="messagesTitle"><h1><?php echo $title ?></h1><br /></div>
 <div id="main_pages_wrapper">
-	<a href="../../pages/view/public/home">public : Home</a>&nbsp;|&nbsp;<a href="../../pages/view/sandpit/home">Sandpit</a>&nbsp;|&nbsp;<a href="../../pages/view/help/home">Help</a>
+	<a href="../../pages/view/public/home">public : Home</a>&nbsp;|&nbsp;<a href="../../pages/view/public/sandpit">Sandpit</a>&nbsp;|&nbsp;<a href="../../pages/view/public/help">Help</a>
 	<br />
 	<a href="<?php echo base_url().'index.php/messages/view/'.$username ?>">View unread messages</a>&nbsp;|&nbsp;<a href="<?php echo base_url().'index.php/messages/view_all/'.$username ?>">View all messages</a>&nbsp;|&nbsp;<a id="add_message_form_trigger" href="#add_message_form">Create new message</a>
 </div>
