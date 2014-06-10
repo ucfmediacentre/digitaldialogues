85dc616d2d348225784ca8a1d0860e2851d31e17
Jem Mackay <jem@swarmtv.org>
2014-06-10 14:57:12 +0100

    changes https to http

--- private/codeigniter/application/controllers/recentChanges.php
+++ private/codeigniter/application/controllers/recentChanges.php
@@ -13,7 +13,7 @@ class RecentChanges extends CI_Controller {
 		
 		//Set up simplepie
 		$data['group'] = $this->input->get('group');
-		$feedUrl = "https://digitaldialogues.org/index.php/feed?group=community";
+		$feedUrl = "http://digitaldialogues.org/index.php/feed?group=community";
 		//$feedUrl = base_url().'index.php/feed?group=' . $data['group'];
 		$this->simplepie->set_feed_url($feedUrl);
 		echo "$ feedUrl = "."\n";
