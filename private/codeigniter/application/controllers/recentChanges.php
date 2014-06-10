5634cccef19c60ea40c537db2fc1e995ccae92f5
Jem Mackay <jem@swarmtv.org>
2014-06-10 14:55:24 +0100

    tests out rss feed with https

--- private/codeigniter/application/controllers/recentChanges.php
+++ private/codeigniter/application/controllers/recentChanges.php
@@ -13,8 +13,12 @@ class RecentChanges extends CI_Controller {
 		
 		//Set up simplepie
 		$data['group'] = $this->input->get('group');
-		$feedUrl = base_url().'index.php/feed?group=' . $data['group'];
+		$feedUrl = "https://digitaldialogues.org/index.php/feed?group=community";
+		//$feedUrl = base_url().'index.php/feed?group=' . $data['group'];
 		$this->simplepie->set_feed_url($feedUrl);
+		echo "$ feedUrl = "."\n";
+		print_r($feedUrl);
+		echo "\n";
 		$this->simplepie->set_cache_location(APPPATH.'cache/rss');
 		$this->simplepie->set_cache_duration(60);
 		$this->simplepie->init();
